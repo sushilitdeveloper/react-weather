ffd2b054bee0cc929a8c5111ff5b2ce9417f5ff6
Sushil Kumar <sushilitdeveloper@gmail.com>
2021-08-19 18:49:34 +0530

    added footer

--- src/App.js
+++ src/App.js
@@ -7,9 +7,17 @@ function App() {
   return (
     <div className='App'>
       <Provider store={store}>
-        <MainComponent/>
+        <MainComponent />
 
       </Provider>
+
+      <footer className="fixed bottom-0 w-full flex justify-center px-4 text-gray-100 bg-gray-800">
+        <div className="container py-1">
+          <h1 className="text-center text-md font-bold lg:text-xl">
+                App by - Sushil Kumar
+          </h1>
+        </div>
+      </footer>
     </div>
   );
 }
